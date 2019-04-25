eaae1d1d388c4683365ceb6af0732d729d2e0576
= <2974842462@qq.com>
2019-04-25 19:04:23 +0800

    动画效果，但没有实现调用

--- proj/js/index1.js
+++ proj/js/index1.js
@@ -14,3 +14,19 @@ function showTime(){
   },1000);
 }
 /**************************/
+function navBarEffect(){
+  var sectorsLi = document.querySelectorAll(".nav-bar li");
+  var sectorsSpan = document.querySelectorAll(".nav-bar li span");
+  for(var i = 0; i < sectorsLi.length; i ++){
+    sectorsLi[i].click=function(){
+      sectorsSpan[i].style.animation="test .3s";
+      sectorsSpan[i].style.animationFillMode="forwards";
+    }
+  }
+}
+function circle(){
+  var cc = document.querySelector(".test");
+  window.setInterval(function(){
+
+  },100);
+}
